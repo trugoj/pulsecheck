9c38e7de78675257e02fecdcd19d0f618e54e90e
Martin Nigsch <martin_nigsch@swissre.com>
2017-11-10 16:19:23 +0100

    remove old

--- pc-back-old/src/services/index.js
+++
@@ -1,7 +0,0 @@
-const users = require('./users/users.service.js');
-const pulse = require('./pulse/pulse.service.js');
-module.exports = function () {
-  const app = this; // eslint-disable-line no-unused-vars
-  app.configure(users);
-  app.configure(pulse);
-};
